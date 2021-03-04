3217d1b543337110f80e07fc8211388eb7e03bbd
Anvay Mishra <201851026@iiitvadodara.ac.in>
2021-03-04 20:53:14 +0530

    change about text

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -7,12 +7,10 @@ function About() {
       <p className="About__root--title">About</p>
       <section className="About--container">
         <section className="About--textContainer">
-          Alfaaz is the Annual Literature Festival of Indian Institute of Information Technology, Vadodara. It is organized by the Literature Committee IIIT Vadodara and serves as a platform for literarians to showcase their knowledge, creativity and skills.
+          Now in it's third year, the Literature Committee IIIT Vadodara celebrates Alfaaz, the Annual Literature Festival. We invite you all to share and discuss the pleasures and challenges of written and verbal expression with a great blend -- from fiction and nonfiction to poetry and spoken-word performance.
 
 Alfaaz has a complete dose of mainstream events including open mic, poetry & microstory writing, fan fiction , spell bee and fun events such as guess the gibberish and many more. 
-The festival offers a unique opportunity to hear some of the greatest contemporary literary stalwarts together on a single platform that brings diverse thoughts & opinions, and upholds freedom of thought & expression during the five-day mega event.
-
-Join us to commemorate literature, join us, in Alfaaz!
+The festival offers a unique opportunity to hear some of the greatest contemporary literary stalwarts together on a single platform that brings diverse thoughts & opinions, and upholds freedom of thought & expression during the five-day mega event. It does not matter if you are a reader or writer or a speaker, we have something special for all of you. With 25k+ prizes up for grab, we assure you that this will be fun for you.
         </section>
         <div className="About--imgContainer">
           <img src="/media/booklogo.svg" alt="books-logo" />
