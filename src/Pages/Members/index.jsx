7ca4ccd5739a1f26beaae47d0d0df41ac742ca7c
Aditya Prakash <p.aditya.198@gmail.com>
2021-03-04 19:52:30 +0530

    Members page updated :sparkles:

--- src/Pages/Members/index.jsx
+++ src/Pages/Members/index.jsx
@@ -1,28 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Member from "../../Component/Member";
 import Navbar from "../../Component/Navbar";
 import "./Members.css";
 import Footer from "../Footer";
 import NavigationHelper from "../NavigationHelper";
-import { members } from "../../json/members.json";
+import members from "../../json/members.json";
 
 const Members = () => {
-  const [currentTab, setCurrentTab] = useState(0);
-
+  const [currentTab, setCurrentTab] = useState("Web Team");
+  const [activeTeam, setActiveTeam] = useState(
+    members.filter((ele) => ele.team === "Web Team")
+  );
   const changeCurrentTab = (tab) => {
     setCurrentTab(tab);
   };
 
+  useEffect(() => {
+    setActiveTeam(members.filter((ele) => ele.team === currentTab));
+    return () => {};
+  }, [currentTab]);
+
+  const getTeams = (arr) => {
+    const tempTeams = arr.map((ele) => ele.team);
+    return [...new Set(tempTeams)];
+  };
   return (
     <React.Fragment>
       <NavigationHelper />
       <Navbar
-        teamNames={"web"}
+        teamNames={getTeams(members)}
         currentTab={currentTab}
         changeCurrentTab={changeCurrentTab}
       />
       <div className="members">
-        {members.map((ele) => {
+        {activeTeam.map((ele) => {
           return (
             <Member
               imgSrc={ele.img}
